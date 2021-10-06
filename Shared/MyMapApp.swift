5d21ec5310977b6701ca17cc3793bbaa02c06581
naru-77 <naru.fumi@icloud.com>
2021-10-07 00:58:15 +0900

    Initial Commit

---
+++ Shared/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  Shared
+//
+//  Created by ナルセ　ユウキ on 2021/10/07.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
